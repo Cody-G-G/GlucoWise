163b67a0b363aa1f82b596c30a2f2faf441232c2
Cody-G-G <Codrin.Gidei@gmail.com>
2016-12-06 02:40:46 +0000

    initial commit - capabilities: scan devices / connect / receive data through notify (currently printed to console)

---
+++ android/app/src/main/java/com/glucowise/BluetoothManagerPackage.java
@@ -0,0 +1,36 @@
+package com.glucowise;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.JavaScriptModule;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by iHack1337 on 12/1/2016.
+ */
+class BluetoothManagerPackage implements ReactPackage {
+
+    @Override
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
+        List<NativeModule> modules = new ArrayList<>();
+
+        modules.add(new BluetoothManagerModule(reactContext));
+
+        return modules;
+    }
+}
---
+++ index.android.js
@@ -0,0 +1,229 @@
+'use strict';
+
+import React, {Component} from 'react';
+import {
+    AppRegistry,
+    Navigator,
+    View,
+    Text,
+    ListView,
+    ScrollView,
+    StyleSheet,
+    PermissionsAndroid,
+    NetInfo,
+    NativeModules
+} from 'react-native';
+const btManagerNative = NativeModules.BluetoothManagerModule;
+import {List, ListItem, Content, Container, Thumbnail, Button, Icon} from 'native-base';
+import log from './helpers/logger';
+import bleManager from 'react-native-ble';
+import LocationServicesDialogBox from "react-native-android-location-services-dialog-box";
+import Toast from 'react-native-root-toast';
+
+
+let scannedDevices = new Set();
+let stateManipulator = {};
+
+class GlucoWise extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            pizza: '\ud83c\udf55',
+            deviceList: [],
+            connectedUUID: ''
+        }
+    }
+
+    componentWillMount() {
+        stateManipulator.updateDeviceList = (devices) => {
+            this.setState({
+                deviceList: devices
+            });
+        };
+
+        stateManipulator.updateConnectedDevice = (connectedId) => {
+            this.setState({
+                connectedUUID: connectedId
+            });
+        }
+    }
+
+    render() {
+        return (
+            <View style={{
+                flex: 1,
+                justifyContent: 'space-around',
+                alignItems: 'center'
+            }} refreshing>
+
+                <View style={{flex: 1, paddingTop: 20}} refreshing>
+                    <Button onPress={scanDevices} large rounded style={{backgroundColor: 'cornflowerblue'}}>
+                        <Icon theme={{iconFamily: "MaterialIcons"}} name="bluetooth"/>
+                    </Button>
+                </View>
+
+                <View style={{flex: 4, alignSelf: 'stretch'}} refreshing>
+                    <ListItem itemDivider><Text>Found Devices</Text></ListItem>
+                    <List
+                        dataArray={[...this.state.deviceList]}
+                        renderRow={
+                            (item) => (
+                                <ListItem itemDivider
+                                          onPress={() => connectToDevice(item)}
+                                          style={{backgroundColor: 'cornflowerblue'}} button>
+                                    <Text style={{color: 'white'}}>
+                                        {item.advertisement.localName}&nbsp;-&nbsp;
+                                        {item.id}{this.state.connectedUUID === item.id && " - CONNECTED"}
+                                    </Text>
+                                </ListItem>)
+                        }
+                    />
+                </View>
+            </View>
+        )
+    }
+
+    componentDidMount() {
+        requestLocationCoarsePermission();
+        onDiscover();
+        onStateChange();
+        onScanStart();
+        onScanStop();
+    }
+}
+
+async function connectToDevice(peripheral) {
+    const extendedDeviceId = peripheral.advertisement.localName + " - " + peripheral.id;
+    peripheral.connect((error) => {
+        if (error) {
+            log("ERROR on connection with " + extendedDeviceId + ": " + error);
+        } else {
+            log("CONNECTED");
+            peripheral.discoverSomeServicesAndCharacteristics(null, ["0000ffe400001000800000805f9b34fb"], (error, services, characteristics) => {
+                if (error)
+                    log("ERROR: " + error);
+                else {
+                    log("CHARACTERISTICS: " + characteristics[0]);
+                    // characteristic.once('notify', (state) => log("STATE: " + state));
+                    characteristics[0].subscribe((error) => {
+                        log("ERROR ON SUBSCRIPTION: " + error);
+                    });
+                    characteristics[0].on('data', (data, isNotification) => log("DATA: " + data));
+                }
+            });
+        }
+    });
+}
+
+async function scanDevices() {
+    btManagerNative.enable((enabled, error) => {
+        log("BT ENABLED: " + enabled);
+        log("BT ENABLE ERROR: " + error);
+        if (enabled) {
+            requestLocationServices()
+                .then((result) => {
+                    if (result) {
+                        bleManager.state = "poweredOn";
+                        bleManager.emit("stateChange", "poweredOn");
+                        bleManager.startScanning();
+                    }
+                })
+                .catch((error) => log("Location Services Request Rejected with: " + error));
+        }
+    });
+}
+
+async function showToast(message) {
+    Toast.show(message, {
+        duration: Toast.durations.LONG,
+        position: Toast.positions.CENTER,
+        shadow: true,
+        animation: true,
+        hideOnPress: true,
+        delay: 0
+    });
+}
+
+async function requestLocationCoarsePermission() {
+    try {
+        const granted = await
+            PermissionsAndroid.requestPermission(
+                PermissionsAndroid.PERMISSIONS.ACCESS_COARSE_LOCATION,
+                {
+                    'title': 'Location permission',
+                    'message': 'In order to use bluetooth, the app needs Location permissions.'
+                }
+            );
+        log("Permission LOCATION_COARSE granted: " + granted);
+    } catch (err) {
+        log("ERROR on requesting LOCATION_COARSE_LOCATION permission: " + err);
+    }
+}
+
+async function onDiscover() {
+    bleManager.on('discover', (peripheral) => {
+            log("Found device: " + peripheral);
+            onConnection(peripheral);
+            onDisconnection(peripheral);
+            scannedDevices.add(peripheral);
+            stateManipulator.updateDeviceList([...scannedDevices]);
+        }
+    );
+}
+
+async function onStateChange() {
+    bleManager.on('stateChange', (state) => {
+        log("Noble state changed to: " + state);
+    });
+}
+
+async function onScanStart() {
+    bleManager.on('scanStart', () => {
+        log("Device scan started");
+        setTimeout(function () {
+            bleManager.stopScanning();
+        }, 10000);
+    });
+}
+
+async function onScanStop() {
+    bleManager.on('scanStop', () => {
+        log("Device scan stopped");
+    });
+}
+
+async function onConnection(peripheral) {
+    peripheral.on('connect', () => {
+        log("Connecting to " + peripheral.advertisement.localName + " - " + peripheral.id);
+        stateManipulator.updateConnectedDevice(peripheral.id)
+    });
+}
+
+async function onDisconnection(peripheral) {
+    peripheral.on('disconnect', () => {
+        log("Disconnecting from " + peripheral.advertisement.localName + " - " + peripheral.id);
+        stateManipulator.updateConnectedDevice("");
+    });
+}
+
+async function requestLocationServices() {
+    log("Requesting Location Services");
+    return new Promise((resolve, reject) => {
+        LocationServicesDialogBox.checkLocationServicesIsEnabled({
+            message: "<h2>Use Location ?</h2>This app wants to change your device settings:<br/><br/>Use GPS, Wi-Fi, and cell network for location<br/><br/><a href='#'>Learn more</a>",
+            ok: "YES",
+            cancel: "NO"
+        })
+            .then((success) => {
+                log("Location Service Request Success: " + success);
+                resolve(true)
+            })
+            .catch((error) => {
+                log("Location Service Request ERROR: " + error.message);
+                reject(false);
+            });
+    });
+}
+
+AppRegistry.registerComponent('GlucoWise', () => GlucoWise);
