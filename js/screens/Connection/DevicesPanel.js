32d79c65180239f027d8b7581d2ecd76e41e43f6
Cody-G-G <Codrin.Gidei@gmail.com>
2017-02-03 15:39:03 +0000

    moved determining device connection button color / text to own functions

--- js/screens/Connection/DevicesPanel.js
+++ js/screens/Connection/DevicesPanel.js
@@ -13,27 +13,39 @@ class DevicesPanel extends Component {
     render() {
         return (
             <View style={styles.devicesPanel}>
+
                 <ListItem itemDivider>
                     <Text style={styles.deviceListHeader}>Found Devices</Text>
                 </ListItem>
+
                 <ListView dataSource={this.props.scannedDevices} enableEmptySections={true} renderRow={(rowData) =>
                                 <View style={styles.device}>
                                     <Text style={styles.deviceDescription}>
                                             <B>Name:</B> {JSON.parse(rowData).name}{"\n"}
                                             <B>Id:</B> {JSON.parse(rowData).id}
                                     </Text>
+
                                     <TouchableOpacity
-                                        style={StyleSheet.flatten([styles.deviceButton, {backgroundColor: this.props.connectedDevices.includes(JSON.parse(rowData).id) ? 'firebrick' : 'green'}])}
-                                        onPress={() => this.props.onPress(JSON.parse(rowData))} disabled={this.props.devicesTogglingConnection.includes(JSON.parse(rowData).id)}>
-                                            <Text style={styles.deviceButtonText}>
-                                                {this.props.connectedDevices.includes(JSON.parse(rowData).id) ? "Disconnect" : "Connect"}
-                                            </Text>
+                                        style={StyleSheet.flatten([styles.deviceButton, {backgroundColor: this.buttonColor(rowData)}])}
+                                        onPress={() => this.props.onPress(JSON.parse(rowData))}
+                                        disabled={this.props.devicesTogglingConnection.includes(JSON.parse(rowData).id)}>
+
+                                            <Text style={styles.deviceButtonText}>{this.buttonText(rowData)}</Text>
+
                                     </TouchableOpacity>
                                 </View>
-                            }
+                         }
                 />
             </View>
         );
+    }
+
+    buttonColor(rowData) {
+        return this.props.connectedDevices.includes(JSON.parse(rowData).id) ? 'firebrick' : 'green';
+    }
+
+    buttonText(rowData) {
+        return this.props.connectedDevices.includes(JSON.parse(rowData).id) ? "Disconnect" : "Connect";
     }
 }
 
