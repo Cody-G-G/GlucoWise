d226fc9256113a31da0702c43133cd68bf13c8db
Cody-G-G <Codrin.Gidei@gmail.com>
2017-03-07 22:55:28 +0000

    updated test & added snap for DevicesPanel

--- js/screens/Connection/test/DevicesPanel.test.js
+++ js/screens/Connection/test/DevicesPanel.test.js
@@ -12,11 +12,19 @@ jest.mock('ListView', () => {
 import DevicesPanel from "../DevicesPanel";
 import React from 'react';
 import 'react-native';
+import {ListView} from 'react-native';
 import renderer from 'react-test-renderer';
 
 test('DevicesPanel - renders correctly', () => {
+    const device1 = {name: "DEVICE_NAME", id: "DEVICE_ID"};
+    const device2 = {name: "DEVICE_NAME2", id: "DEVICE_ID2"};
+    const device3 = {name: "DEVICE_NAME3", id: "DEVICE_ID3"};
+    const scannedDevices = (new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2})).cloneWithRows([
+        device1, device2, device3
+    ]);
+
     const tree = renderer.create(
-        <DevicesPanel/>
+        <DevicesPanel scannedDevices={scannedDevices}/>
     ).toJSON();
 
     expect(tree).toMatchSnapshot();
